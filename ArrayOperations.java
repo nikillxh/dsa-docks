2887623cfa41a89690ce03950fdc4388e4a8269c
nikillxh <njnikhilesh17@gmail.com>
2025-01-15 01:12:15 +0530

    ArrayOperations.java

--- ArrayOperations.java
+++ ArrayOperations.java
@@ -1,9 +1,28 @@
 public class ArrayOperations {
     public static void printArray(int[] arr) {
+        System.out.print("{");
         for (int i=0; i< arr.length; i++) {
-            System.out.print(arr[i] + " ");
+            if (i == arr.length-1) {
+                System.out.print(arr[i]);
+            } else {System.out.print(arr[i] + ",");};
         }
-        System.out.print("\n");
+        System.out.print("}\n");
+    }
+
+    public static void printMatrix(int[][] arr) {
+        System.out.print("{");
+        for (int i=0; i< arr.length; i++) {
+            System.out.print("{");
+            for (int j = 0; j < arr[0].length; j++) {
+                if (j == arr[0].length-1) {
+                    System.out.print(arr[i][j]);
+                } else {System.out.print(arr[i][j] + ",");};
+            }
+            if (i == arr.length-1) {
+                System.out.print("}");
+            } else {System.out.print("}\n");};
+        }
+        System.out.print("}\n");
     }
     
     public static int FindMax(int[] arr) {
@@ -80,14 +99,119 @@ public class ArrayOperations {
         }
     }
 
+    // Prime factor sudoku validation
+    public static boolean isValidSudoku(int[][] board) {
+        int[] prime = {1, 2, 3, 5, 7, 11, 13, 17, 19, 23};
+        //row check
+        for (int i = 0; i < board.length; i++) {
+            int factor = 1;
+            for (int j = 0; j < board.length; j++) {
+                if ((board[i][j] != 0) && (factor % prime[board[i][j]] != 0)) {
+                    factor *= prime[board[i][j]];
+                } else if ((board[i][j] != 0) && (factor % prime[board[i][j]] == 0)) {
+                    return false;
+                }
+            }
+        }
+
+        //column check
+        for (int i = 0; i < board.length; i++) {
+            int factor = 1;
+            for (int j = 0; j < board.length; j++) {
+                if ((board[j][i] != 0) && (factor % prime[board[j][i]] != 0)) {
+                    factor *= prime[board[j][i]];
+                } else if ((board[j][i] != 0) && (factor % prime[board[j][i]] == 0)) {
+                    return false;
+                }
+            }
+        }
+
+        //block check
+        for (int pad = 0; pad < 7; pad += 3){
+            int factor = 1;
+            for (int i = 0 + pad; i < 3 + pad; i++) {
+                for (int j = 0 + pad; j < 3 + pad; j++) {
+                    if ((board[i][j] != 0) && (factor % prime[board[i][j]] != 0)) {
+                        factor *= prime[board[i][j]];
+                    } else if ((board[i][j] != 0) && (factor % prime[board[i][j]] == 0)) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    // Kadane's Algorithm
+    public static int maxSubArrSum(int[] arr) {
+        int res = arr[0];
+        int maxEnding = arr[0]; 
+
+        for (int element: arr) {
+            if (maxEnding < element) {
+                maxEnding = element;
+            } else {maxEnding += element;}
+
+            if (res < maxEnding) {
+                res = maxEnding;
+            }
+        }
+        return res;
+    }
+
+    public static int[][] gameOfLife(int[][] matrix) {
+        // Time complexity complexity *_*
+        int[] nav = {-1,0,1};
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                int kins = 0;
+                for (int x: nav) {
+                    for (int y: nav) {
+                        if ((x == 0 && y == 0) || i+x < 0 || i+x == matrix.length || j+y < 0 || j+y == matrix[0].length) {}
+                        else {
+                            kins += matrix[i+x][j+y];
+                        }
+                    }
+                }
+                if (kins == 3) {matrix[i][j] = 1;}
+                else if (kins < 2 || kins > 3) {matrix[i][j] = 0;}
+            }
+        }
+        return matrix;
+    }
+
     public static void main(String[] args) {
         int[] arr = {1, 3, 4, 6, 31, 2};
         int[] sort = {1, 2, 3};
         int[] sortThree = {2, 1, 3, 2, 1, 3, 2};
+        int[][] board = {{7, 0, 0, 0, 0, 0, 2, 0, 0},   
+            {4, 0, 5, 0, 0, 0, 0, 0, 3},   
+            {0, 4, 0, 2, 0, 1, 0, 0, 0},   
+            {3, 0, 0, 1, 8, 0, 0, 9, 7},   
+            {0, 0, 9, 0, 7, 0, 6, 0, 0},   
+            {6, 5, 0, 0, 3, 2, 0, 0, 1},   
+            {0, 0, 0, 4, 0, 9, 0, 0, 0},   
+            {5, 0, 0, 0, 0, 0, 1, 0, 6},   
+            {0, 0, 6, 0, 0, 0, 0, 0, 8}   
+        };  
+        int[] findMaxSum = {2, -1, 3, 4, -6, 7, -9, 8};
+        int[][] matrixOfLife = {{1, 0, 0, 0, 0, 0, 1, 0, 0},
+            {1, 0, 1, 0, 0, 0, 0, 0, 1},
+            {0, 1, 0, 1, 0, 1, 0, 0, 0},
+            {1, 0, 0, 1, 1, 0, 0, 1, 1},
+            {0, 0, 0, 0, 1, 0, 1, 0, 0},
+            {1, 1, 0, 0, 1, 1, 0, 0, 1},
+            {0, 0, 0, 1, 0, 1, 0, 0, 0},
+            {1, 0, 0, 0, 0, 0, 1, 0, 1},
+            {0, 0, 1, 0, 0, 0, 0, 0, 1}
+        };
         System.out.println(FindMax(arr));
         System.out.println(isSorted(sort));
         printArray(sortThree);
         threeSort(sortThree);
         printArray(sortThree);
+        System.out.println(isValidSudoku(board));
+        System.out.println(maxSubArrSum(findMaxSum));
+        printMatrix(gameOfLife(matrixOfLife));
     }
 }
