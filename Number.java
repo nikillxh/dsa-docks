c75b9e32ff97333dfc5ada99240123655be7928e
nikillxh <njnikhilesh17@gmail.com>
2025-01-20 23:57:45 +0530

    Number!!

--- Number.java
+++ Number.java
@@ -1,5 +1,3 @@
-
-
 public class Number {
     int sign;
     Digits num;
@@ -38,7 +36,7 @@ public class Number {
         number.num = pointer;
         num /= 10;
 
-        while (num != 0 && num > 1) {
+        while (num != 0 && num > 0) {
             pointer.next = new Digits(num % 10);
             num /= 10;
             pointer = pointer.next;
@@ -64,9 +62,141 @@ public class Number {
         else {return -1;}
     }
 
+    public static int outputAsInteger(Number num) {
+        Digits list = num.num;
+        int integer = 0;
+        int factor = 1;
+
+        while (list.next != null && factor < 1000000000) {
+            integer += list.value * factor;
+            factor *= 10;
+            list = list.next;
+        }
+        if (factor < 1000000000) {integer += list.value * factor;}
+
+        return integer;
+    }
+
+    // Addition. F in the terminal
+    public static Number addition(Number x, Number y) {
+        Number sum = new Number();
+        sum.num = new Digits(0);
+        Digits pointer_sum = sum.num, pointer_x = x.num, pointer_y = y.num;
+        int carry = 0;
+
+        while (pointer_x.next != null || pointer_y.next != null) {
+            pointer_sum.value = (pointer_x.value * x.sign) + (pointer_y.value * y.sign) + carry;
+            if (pointer_sum.value > 9) {
+                pointer_sum.value -= 10;
+                carry += 1;
+            } else if (pointer_sum.value < 0) {
+                pointer_sum.value += 10;
+                carry -= 1;
+            } else {
+                carry = 0;
+            }
+
+            if (pointer_x.next != null && pointer_y.next != null) {
+                pointer_sum.next = new Digits(0);
+                pointer_sum = pointer_sum.next;
+            }
+
+            if (pointer_x.next == null || pointer_y.next == null) {break;}
+            else {pointer_x = pointer_x.next;}
+            if (pointer_y.next == null) {break;}
+            else {pointer_y = pointer_y.next;}
+
+        }
+
+        if (pointer_x.next == null && pointer_y.next == null) {
+            // pointer_sum.next = new Digits(0);
+            // pointer_sum = pointer_sum.next;
+            pointer_sum.value = (pointer_x.value * x.sign) + (pointer_y.value * y.sign) + carry;
+            if (pointer_sum.value > 9) {
+                pointer_sum.value -= 10;
+                carry += 1;
+            } else if (pointer_sum.value < 0) {
+                pointer_sum.value += 10;
+                carry -= 1;
+            } else {
+                carry = 0;
+            }
+        }
+
+        while (pointer_x.next != null) {
+            pointer_sum.next = new Digits(0);
+            pointer_sum = pointer_sum.next;
+            pointer_x = pointer_x.next;
+
+            pointer_sum.value = pointer_x.value * x.sign + carry;
+            if (pointer_sum.value > 9) {
+                pointer_sum.value -= 10;
+                carry += 1;
+            } else if (pointer_sum.value < 0) {
+                pointer_sum.value += 10;
+                carry -= 1;
+            } else {
+                carry = 0;
+            }
+            if (pointer_x.next == null) {break;}
+            else {
+                pointer_x = pointer_x.next;
+                if (pointer_x.next == null) {
+                pointer_sum.next = new Digits(0);
+                pointer_sum = pointer_sum.next;
+                pointer_sum.value = pointer_x.value * x.sign + carry;
+                } 
+            }
+        }
+
+        while (pointer_y.next != null) {
+            pointer_sum.next = new Digits(0);
+            pointer_sum = pointer_sum.next;
+            pointer_y = pointer_y.next;
+
+            pointer_sum.value = pointer_y.value * y.sign + carry;
+            if (pointer_sum.value > 9) {
+                pointer_sum.value -= 10;
+                carry += 1;
+            } else if (pointer_sum.value < 0) {
+                pointer_sum.value += 10;
+                carry -= 1;
+            } else {
+                carry = 0;
+            }
+            if (pointer_y.next == null) {break;}
+            else {
+                pointer_y = pointer_y.next;
+                if (pointer_y.next == null) {
+                pointer_sum.next = new Digits(0);
+                pointer_sum = pointer_sum.next;
+                pointer_sum.value = pointer_y.value * y.sign + carry;
+                } 
+            }
+        }
+
+        if (pointer_sum.value < 0) {
+            pointer_sum.value *= -1;
+            sum.sign = -1;
+        } else if (pointer_sum.value > 0) {
+            sum.sign = 1;
+        } else {sum.sign = 1;}
+
+        if (carry != 0) {pointer_sum.next = new Digits(1);}
+        if(pointer_sum.value == 0) {
+            pointer_sum = null;
+        }
+
+        return sum;
+    }
+
     public static void main(String[] args) {
-        Number x = convertToL(2332);
-        Number y = convertToL(-113);
+        Number x = convertToL(2323);
+        Number y = convertToL(-3134);
         System.out.println(compareNumber(x, y));
+        printNumber(addition(x, y));
+
+        Number asInt = convertToL(23342);
+        System.out.println(outputAsInteger(asInt));
     }
 }
